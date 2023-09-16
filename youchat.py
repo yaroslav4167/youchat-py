a6f407b1b4623e5e892656978789521d0ee2a9bd
Yaroslavik <yaroslav4167@mail.ru>
2023-09-16 22:40:39 +0200

    Highlighted challenge solver in code

--- youchat.py
+++ youchat.py
@@ -34,12 +34,11 @@ def you_message(text: str, out_type: str = 'json', timeout: int = 20):
             except Exception:
                 pass
 
-            # Try to easy solve captcha challenge
+            # START Try to easy solve captcha challenge
             try:
                 if sb.assert_element('iframe'):
                     sb.switch_to_frame("iframe")
                     sb.find_element(".ctp-checkbox-label", timeout=1).click()
-                    # sb.save_screenshot('sel2.png') # Debug
             except Exception:
                 result['error'] = 'Selenium was detected! Try again later. Captcha not solved automaticly.'
             finally:
@@ -49,6 +48,7 @@ def you_message(text: str, out_type: str = 'json', timeout: int = 20):
             if time.time() > timeout_delta:
                 # sb.save_screenshot('sel-timeout.png') # Debug
                 result['error'] = 'Timeout while getting data from Selenium! Try again later.'
+            # END Try to easy solve captcha challenge
 
         res_message = ""
         for line in data.split("\n"):
@@ -73,8 +73,7 @@ def main_cli():
     parser.add_argument(
         '-timeout', '-t', help="Timeout to wait response", default=20, type=int)
     args = parser.parse_args()
-    text = args.MESSAGE
-    print(you_message(text, args.out_type, args.timeout))
+    print(you_message(args.MESSAGE, args.out_type, args.timeout))
 
 
 if __name__ == '__main__':
